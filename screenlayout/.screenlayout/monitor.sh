4182184e372067080726e26aa978428da35c5e41
Jesper Lundberg <jesper.lundberg@knowit.se>
2022-10-10 09:50:41 +0200

    Add screenlayout

---
+++ screenlayout/.screenlayout/monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output HDMI4 --off --output HDMI5 --off --output VIRTUAL1 --off
