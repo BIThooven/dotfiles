873c2cf265e1c9184903929561750c14dbe9f054
Jesper Lundberg <jesper.lundberg@knowit.se>
2022-10-19 13:52:56 +0200

    Add config.fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+thefuck --alias | source
+
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_date_format "+%H:%M"
+set -g theme_display_git no
