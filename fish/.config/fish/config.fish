ac86d756788d96f592e84173510bf325aa38c443
Jesper Lundberg <jesper.lundberg@knowit.se>
2023-01-18 11:04:12 +0100

    Set vim as default editor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,10 @@ end
 
 thefuck --alias | source
 
+# Settings for theme
 set -g theme_use_abbreviated_branch_name no
 set -g theme_date_format "+%H:%M"
 set -g theme_display_git_branch yes
+
+# Set environment variable
+set -gx EDITOR nvim
