f09b357c703c5ab2ada4cd287525fc840cbad43a
Jesper Lundberg <jesper.lundberg@knowit.se>
2023-03-29 13:33:15 +0200

    Show master in prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ end
 thefuck --alias | source
 
 # Settings for theme
+set -g theme_display_git_default_branch yes
 set -g theme_use_abbreviated_branch_name no
 set -g theme_date_format "+%H:%M"
 set -g theme_display_git_branch yes
