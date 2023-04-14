2ac7040ec69db6d03ae64ab7290cd73531bd37cb
Jesper Lundberg <jesper.lundberg@knowit.se>
2023-04-14 16:54:31 +0200

    Add nvchad + custom set up

---
+++ nvchad/.config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,15 @@
+local M = {}
+M.ui = {
+	theme = "catppuccin",
+	nvdash = {
+		load_on_startup = true,
+	},
+}
+
+-- read the custom plugins
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require("custom.mappings")
+
+return M
