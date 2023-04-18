7d1e5b129cce11ceb1b93f518d23ba436bd74182
Jesper Lundberg <jesper.lundberg@knowit.se>
2023-04-18 13:41:23 +0200

    Change keymap and strategy for dotnet debugging of unit tests

--- nvchad/.config/nvim/lua/custom/mappings.lua
+++ nvchad/.config/nvim/lua/custom/mappings.lua
@@ -98,7 +98,6 @@ M.neotest = {
 		["<leader>tt"] = {
 			function()
 				require("neotest").run.run()
-				require("neotest").summary.open()
 			end,
 			"run neastest test",
 		},
@@ -109,9 +108,17 @@ M.neotest = {
 			end,
 			"run tests in current file",
 		},
-		["<leader>ttd"] = {
+		["<leader>tdb"] = {
 			function()
-				require("neotest").run.run({ strategy = "dap" })
+        -- netcoredbg needs special strategy
+				if vim.bo.filetype == "cs" then
+					require("neotest").run.run({
+						strategy = require("neotest-dotnet.strategies.netcoredbg"),
+						is_custom_dotnet_debug = true,
+					})
+				else -- for all others, use regular dap strategy
+					require("neotest").run.run({ strategy = "dap" })
+				end
 			end,
 			"debug nearest test",
 		},
