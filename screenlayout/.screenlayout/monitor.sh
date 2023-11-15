f34eb483e454852305bdcf0b85587c792587018e
Jesper Lundberg <jesper.lundberg@knowit.se>
2023-11-15 08:40:13 +0100

    Update monitor script

--- screenlayout/.screenlayout/monitor.sh
+++ screenlayout/.screenlayout/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output HDMI4 --off --output HDMI5 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP3 --off --output DP4 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output HDMI4 --off --output HDMI5 --off --output VIRTUAL1 --off
